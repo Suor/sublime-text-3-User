f47960af899961fc451132237ececedd757a3946
Alexander Schepanovski <suor.web@gmail.com>
2015-04-10 13:29:28 +0700

    Add toggle_white_space macro

--- macros.py
+++ macros.py
@@ -20,3 +20,14 @@ class ToggleLayoutCommand(sublime_plugin.WindowCommand):
                 "rows": [0.0, 1.0],
                 "cells": [[0, 0, 1, 1]]
             })
+
+
+class ToggleWhiteSpaceCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        settings = self.view.settings()
+        if settings.get("draw_white_space", "all") != "all":
+            settings.set("draw_white_space", "all")
+            settings.set("trim_trailing_white_space_on_save", False)
+        else:
+            settings.set("draw_white_space", "none")
+            settings.set("trim_trailing_white_space_on_save", True)
